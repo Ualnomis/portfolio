28bbed8dece81b76d1365af9cc04be0c557aaef0
Ualnomis <ualnamnuhc@gmail.com>
2024-05-23 13:46:13 -0400

    style(lint): style lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   {
     ...pluginJs.configs.recommended,
     settings: {
-      react: { version: "detect" },
+      react: { version: 'detect' },
     },
   },
   eslint.configs.recommended,
