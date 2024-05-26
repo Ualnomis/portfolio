4f393fc9266c0b1bae536cc179ad2cd59da9e141
Ualnomis <ualnamnuhc@gmail.com>
2024-05-25 21:50:00 -0400

    feat: sketch footer section

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import MaxWidthWrapper from '../MaxWidthWrapper';
+
+const Footer = () => {
+  return <Wrapper>© {new Date().getFullYear()} Simon Lau</Wrapper>;
+};
+
+const Wrapper = styled(MaxWidthWrapper)`
+  height: 96px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Footer;
